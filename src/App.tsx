8d747517d955a48b0bd3fbf0ab5d61901a0a98b0
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-06-19 10:18:27 +0100

    Added final style

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,53 @@
-import './App.css'
+import './index.css'
 import arrow from './images/icon-arrow.svg'
 function App() {
 
   return (
     <>
       <main
-        className='h-[340px] w-[60rem] bg-offWhite p-9'
+        className='h-[full] w-[45rem] bg-offWhite p-12 space-y-3 rounded-lg'
+        style={{
+          borderBottomRightRadius: '8rem'
+        }}
       >
-        <form action="" className='flex gap-3'>
+        <form action="" >
           <div
-            className='flex flex-col'
+            className='flex gap-3'
           >
-            <label htmlFor="day">Day</label>
-            <input type="text" className="input input-bordered bg-white w-[15rem] h-[50px] max-w-xs" />
+            <div
+              className='flex flex-col'
+            >
+              <label htmlFor="day" className='text-[#646464]'>Day</label>
+              <input type="text" className="input input-bordered bg-white w-[10rem] h-[50px] max-w-xs" />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor="month" className='text-[#646464]'>MONTH</label>
+              <input type="text" className="input input-bordered bg-white w-[10rem] h-[50px] max-w-xs" />
+            </div>
+            <div className='flex flex-col'>
+              <label htmlFor="Year" className='text-[#646464]'>YEAR</label>
+              <input type="text" className="input input-bordered bg-white w-[10rem] h-[50px] max-w-xs" />
+            </div>
           </div>
-          <div className='flex flex-col'>
-            <label htmlFor="month">MONTH</label>
-            <input type="number" className="border border-lightGrey" />
-          </div>
-          <div className='flex flex-col'>
-            <label htmlFor="Year">YEAR</label>
-            <input type="number" className="border border-lightGrey" />
-          </div>
-          <hr />
-          <button type='submit' className="btn btn-secondary">Secondary</button>
-        </form>
-        <div>
-          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>38</span>Years</h1>
-          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>3</span>months</h1>
-          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>26</span>Days</h1>
+
+          <br />
+          <section
+            className='flex flex-col items-center md:flex-row'
+          >
+            <div className='w-[90%]'>
+              <hr className='w-[90%] h-[3px] bg-black' />
+            </div>
+            <button type="submit">
+              <img src={arrow} className="m-4 md:m-0 rounded-[100%] bg-purple px-12 py-12 flex justify-center items-center" alt="" />
+            </button>
+          </section>
+        </form >
+        <div className='w-full'>
+          <h1 className='text-[54px] text-black'><span className='text-purple text-[54px]'>34</span> Years</h1>
+          <h1 className='text-[54px] text-black'><span className='text-purple text-[54px]'>3</span> Month</h1>
+          <h1 className='text-[54px] text-black'><span className='text-purple text-[54px]'>26</span> Days</h1>
         </div>
-      </main>
+      </main >
 
     </>
   )
