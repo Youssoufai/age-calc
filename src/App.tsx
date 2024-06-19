0aac51a8a566eefb6ec1694bb81a63fa74ec653e
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-06-19 09:20:50 +0100

    Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
             className='flex flex-col'
           >
             <label htmlFor="day">Day</label>
-            <input type="number" className="border border-lightGrey" />
+            <input type="text" className="input input-bordered bg-white w-[15rem] h-[50px] max-w-xs" />
           </div>
           <div className='flex flex-col'>
             <label htmlFor="month">MONTH</label>
@@ -23,14 +23,12 @@ function App() {
             <input type="number" className="border border-lightGrey" />
           </div>
           <hr />
-          <button type="submit" className='py-2 px-4 bg-black text-white rounded'>
-            Submit
-          </button>
+          <button type='submit' className="btn btn-secondary">Secondary</button>
         </form>
         <div>
-          <h1><span>38</span>Years</h1>
-          <h1><span>3</span>months</h1>
-          <h1><span>26</span>Days</h1>
+          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>38</span>Years</h1>
+          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>3</span>months</h1>
+          <h1 className='text-4xl text-black'><span className='text-purple text-8xl'>26</span>Days</h1>
         </div>
       </main>
 
